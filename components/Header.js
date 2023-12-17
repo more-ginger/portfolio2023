6767fae0567382da0fe750187f7a2c68072dedc0
more-ginger <francesca1morini@gmail.com>
2023-12-17 18:16:59 +0100

    change structure of essay body

--- components/Header.js
+++ components/Header.js
@@ -2,9 +2,9 @@ import Link from 'next/link';
 
 export default function Header({ name }) {
   return (
-    <header className="w-full sticky inset-x-0 top-0 bg-blue-500">
-      <div className="pt-5 pb-5 w-full flex bg-blue-100">
-        <div className="flex w-1/2 bg-blue-200">
+    <header className="w-full sticky inset-x-0 top-0 bg-white backdrop-blur-lg bg-opacity-80">
+      <div className="pt-5 pb-5 w-full flex">
+        <div className="flex w-1/2">
           <div className="flex-none pl-5 pr-5">
             <Link href="/">Read</Link>
           </div>
@@ -14,7 +14,7 @@ export default function Header({ name }) {
             </Link>
           </div>
         </div>
-        <div className="flex w-1/2 justify-end bg-blue-600">
+        <div className="flex w-1/2 justify-end">
           <div className="pl-5 pr-5">
             <p>
               Mail
