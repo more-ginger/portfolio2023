4c23522059100e498e50acb95596e5e23eb9cc32
more-ginger <francesca1morini@gmail.com>
2024-03-01 18:13:26 +0100

    fix dark mode

---
+++ portfolio2023/components/ArrowIcon.js
@@ -0,0 +1,11 @@
+export default function ArrowIcon({ className }) {
+  return (
+    <svg className={className} width="52px" height="34px" viewBox="0 0 52 34">
+      <g id="Page-1" stroke="none" fill="none">
+        <g id="noun-arrow-3771902" transform="translate(0.300000, 0.800000)">
+          <polygon id="Path" points="33.8 31 35.2 32.4 51.4 16.2 35.2 0 33.8 1.4 47.6 15.2 0 15.2 0 17.2 47.6 17.2"></polygon>
+        </g>
+      </g>
+    </svg>
+  );
+}
