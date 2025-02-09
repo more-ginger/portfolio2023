901daa05b916b03c9e557b67769451d31c9a91f1
fmorini <33264532+more-ginger@users.noreply.github.com>
2025-02-09 16:28:40 +0100

    add article data for blog

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,7 @@ export default function Header({ name }) {
 
 
   return (
-    <header className="w-full z-10 sticky inset-x-0 top-0 backdrop-blur-lg bg-opacity-80 dark:bg-opacity-80 dark:bg-gray-900">
+    (<header className="w-full z-10 sticky inset-x-0 top-0 backdrop-blur-lg bg-opacity-80 dark:bg-opacity-80 dark:bg-gray-900">
       <div className="pt-5 pb-5 w-full flex">
         <div className="flex w-2/3">
           <div className="flex-none pl-5 pr-1">
@@ -20,8 +20,8 @@ export default function Header({ name }) {
               Archive
             </Link>
           </div>
-          <div className="flex-none pl-1 pr-1 pl-2 italic pointer-events-none cursor-not-allowed non-active-link">
-            <Link href="/read"><span className="opacity-20 text-sm">Read</span></Link><span className="ml-1 px-2 text-white align-top text-[9px] bg-red-700 rounded-full dark:bg-red-300 dark:text-gray-900">soon</span>
+          <div className="flex-none pl-5 pr-1">
+            <Link href="/read">Journal</Link>
           </div>
         </div>
         <div className="flex w-1/3 justify-end">
@@ -30,6 +30,6 @@ export default function Header({ name }) {
           </div>
         </div>
       </div>
-    </header>
+    </header>)
   );
 }
--- pages/read.js
+++ pages/read.js
@@ -1,34 +1,27 @@
+import { data } from 'autoprefixer';
 import Layout from '../components/Layout';
 import { getPages } from '../utils/mdx-utils';
 
 export default function Index({ pages }) {
-  // console.log(pages[0])
-  const initialEssay = pages.filter((page) => {
-    const ids = page.data.ID
-    ids.find((id) => id === 'initial-essay')
-    return ids.includes('initial-essay')
+  
+  const essayPreviews = pages.map((article, a) => {
+    return {
+      title: article.data.title,
+      date: article.data.date,
+      number: a + 1,
+      filepath: article.filePath
+    }
   })
 
   return (
     <Layout>
-      {/* <SEO title={globalData.name} description={globalData.blogTitle} /> */}
-      <main className="w-full flex-grow pb-48">
-        <div className="m-5 p-1 text-sm bg-red-100 italic rounded border-red-600">
-          <div className="m-5">This is the last published essay. The full list can be found in the archive.</div>
-        </div>
-        <div className="pl-5 pr-5 pt-5 pb-5">
-          <h1 className="text-3xl border-b">
-            {initialEssay[0].data.title}
-          </h1>
-        </div>
-        <div className="pl-5 pr-5 text-base">
-          {initialEssay[0].data.paragraphs.map((par, p) => (
-            <div key={par.paragraph}>
-              <p className="pb-10">{par.paragraph}</p>
-            </div>
+      <div>
+        <div className="essays-container">
+          {essayPreviews.map((preview) => (
+            <div key={preview.filepath}>{preview.title}</div>
           ))}
         </div>
-      </main>
+      </div>
     </Layout>
   );
 }
